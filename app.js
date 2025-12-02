cc52c5f6db90198074c42f7b6a58c516bbcd79db
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-12-02 14:14:26 -0800

    yearbyyear fix

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const pool = mysql2.createPool({
 }).promise();
 const path = import.meta.dirname;
 app.set('view engine', 'ejs');
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
